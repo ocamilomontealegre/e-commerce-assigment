b733e6407dce96423eca29fa7f1bb07930805f13
Camilo Montealegre <ocamilomontealegre@gmail.com>
2023-12-07 23:13:04 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,6 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import https from 'https';
-import fs from 'fs';
 import routes from './src/eccommerceApp/routes.js';
 
 /******* Set app *******/
@@ -21,16 +19,6 @@ app.use(express.json());
 /******* Routes *******/
 app.use('/', routes);
 
-/******* HTTPS Options *******/
-const credentials = {
-  key: fs.readFileSync('./src/certificates/key.pem', 'utf8'),
-  cert: fs.readFileSync('./src/certificates/cert.pem', 'utf8'),
-};
-
-/******* Create HTTPS Server *******/
-const httpsServer = https.createServer(credentials, app);
-
-/******* Start HTTPS Server *******/
-httpsServer.listen(port, () => {
-  console.log(`HTTPS Server is running on port https://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
